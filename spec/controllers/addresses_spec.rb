b053762cac47a96cfbc60cb231fd83ad9df99ea3
Ihar <iharprokop@gmail.com>
2018-03-22 22:54:19 +0300

    add rspec tests 'POST create' for addresses_controller

--- spec/controllers/addresses_spec.rb
+++ spec/controllers/addresses_spec.rb
@@ -22,7 +22,22 @@ describe AddressesController, type: :controller do
     it "assignes new address and render template new" do
       get :new
       expect(assigns(:address)).to be_a_new(Address)
-      expect(response).to render_template(:new)
+      expect(response).to render_template(:new) 
     end
+  end
+
+  describe "POST create" do
+    it "creates new address" do
+      expect do
+        post :create, params: { address: { address_1: "slobodskaya", address_2: "141-140", city: "Minsk", country: "Belarus", zip_code: "220025" } }
+      end.to change { Address.count }.by(1)
+      expect(response).to have_http_status(:redirect)
+    end
+    
+    it "reject empty required fields" do
+      post :create, params: {address: { address_1: "slobodskaya", address_2: "141-140", city: "Minsk", country: "Belarus", zip_code: "" } } 
+      expect(response).to render_template(:new)
+      expect(assigns(:address).errors).not_to be_empty
+    end 
   end
 end
