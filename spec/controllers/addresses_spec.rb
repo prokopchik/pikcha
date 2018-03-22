88462a7c7580d9d6f64a772082b41e0679d80588
Ihar <iharprokop@gmail.com>
2018-03-22 22:23:50 +0300

    add rspec tests 'GET new' for addresses_controller

--- spec/controllers/addresses_spec.rb
+++ spec/controllers/addresses_spec.rb
@@ -10,11 +10,19 @@ describe AddressesController, type: :controller do
   end 
   
   
-  describe "index" do 
+  describe "GET index" do 
     it "assignes addresses and render tameplate index" do
       get :index
       expect(assigns(:addresses).size).to be 1
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET new" do
+    it "assignes new address and render template new" do
+      get :new
+      expect(assigns(:address)).to be_a_new(Address)
+      expect(response).to render_template(:new)
     end
   end
 end
